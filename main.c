a3b71edf219ad83608b78d867e6192004860acb0
mbayedione10 <mbayedione10@gmail.com>
2019-02-04 01:41:11 +0000

    ajout de la partie main

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+
+int main()
+{
+    int choix;
+    do
+    {
+        menu();
+        scanf("%d",&choix);
+        switch (choix)
+        {
+            case 1:
+                 PlusOuMoins(100,1);
+                break;
+
+            case 2:
+                PlusOuMoins(1000,1);
+                break;
+            case 3:
+                PlusOuMoins(10000,1);
+                break;
+            default : printf("entrer un nombre entre 1 et 3 svp");
+        }
+    }while (continuerPartie() == 1);
+    return 0;
+}
