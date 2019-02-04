4fe3e66f45acc1303f38ff8686faa86cdccca944
mbayedione10 <mbayedione10@gmail.com>
2019-02-04 01:35:54 +0000

    ajout du fichier des entetes

---
+++ plusOumoins.h
@@ -0,0 +1,20 @@
+#ifndef PLUSOUMOINS_H_INCLUDED
+#define PLUSOUMOINS_H_INCLUDED
+typedef struct personne
+{
+    char nom[30];
+    char prenom[50];
+    int age;
+    int homme; //homme=1 et femme=0
+    struct personne * suivant;
+}Personne;
+typedef Personne * suivantPersonne;
+
+int continuerPartie();
+void menu ();
+void PlusOuMoins(int maxi, int mini);
+int sommeTableau (int tableau[], int tailleTableau);
+
+
+
+#endif // PLUSOUMOINS_H_INCLUDED
